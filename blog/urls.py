53d6890ec1036ee83aadbfbc2b860b6c47b3fce7
savin ilya <savin15ilya3112@gmail.com>
2025-12-15 00:44:49 +0700

    added urls

---
+++ blog/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+
+    path('profile/<int:user_id>/', views.profile_view, name='profile_view'),
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
+    path('profile/delete/', views.profile_delete, name='profile_delete'),
+
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:post_id>/delete/', views.post_delete, name='post_delete'),
+
+    path('comment/<int:comment_id>/edit/', views.comment_edit, name='comment_edit'),
+    path('comment/<int:comment_id>/delete/', views.comment_delete, name='comment_delete'),
+
+    path('like/<int:post_id>/', views.like_post, name='like_post'),
+]
